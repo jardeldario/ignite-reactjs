138d246fa2a4b606ccdd14471d19fe862c58d8dc
Jardel <jardel.dario@gmail.com>
2022-05-13 23:51:44 -0300

    :grimacing: finalizando consumindo uma api do chapter II

--- dtmoney/src/styles/global.ts
+++ dtmoney/src/styles/global.ts
@@ -4,7 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     :root{
         --red: #e52e4d;
         --blue: #5429CC;
-
+        --green: #33CC95;
         --blue-light: #6933FF;
 
         --text-title: #363F5F;
